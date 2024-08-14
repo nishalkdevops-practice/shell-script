56b0782708d92f6c6146cc872d263cd5eec085e6
nishal <nishalk9399@gmail.com>
2024-08-14 20:08:33 +0530

    conversation

--- 06-datatypes.sh
+++ 06-datatypes.sh
@@ -6,3 +6,4 @@ echo "First person: ${PERSONS[0]}"
 
 #inside the array we can always refer the values with the index and it starts from 0
 
+echo "Display all persons name: ${PERSONS[@]}" #@ specify all 
