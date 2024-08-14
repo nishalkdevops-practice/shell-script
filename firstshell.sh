b8edd132feb84ef757f6dccf4e4244428017d9b9
nishal <nishalk9399@gmail.com>
2024-08-14 16:52:21 +0530

    conversation

--- firstshell.sh
+++ firstshell.sh
@@ -1,7 +1,18 @@
 #!/bin/bash 
 
+PERSON1=RAMESH
+PERSON2=SURESH
+
 echo "Hi this is my first script in shell....."
 
 echo "This is the second file of the script iam writing"
 
 echo "This is third line for testing"
+
+echo "$PERSON1: Hi this  is my first script in shell....."
+
+echo "$PERSON2: OIC its good"
+
+echo "$PERSON1: What about you ? are you developing something or not"
+
+echo "$PERSON2: I'm just doing some timepass no work only njoy"
