0a8ba7b78bdb777dd434fe71f4ca8602dc56b264
nishal <nishalk9399@gmail.com>
2024-08-15 19:37:48 +0530

    pusing the updated 12 script

--- 12-loops-extra.sh
+++ 12-loops-extra.sh
@@ -1,7 +1,53 @@
 #!/bin/bash
 
+
+
+#mimprovements logfiles, colors, validations, functions 
+
+DATE=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$0
+LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR: Please run the script with the root user credentials"
+    exit1
+fi
+
+
+VALIDATE(){
+
+if [ $1 -ne 0 ]
+then
+    echo -e "$2 $R ERROR $N: Installation"
+    exit1
+else
+    echo -e "$2 $G SUCCESSFUL $N: Installation"
+fi
+
+}
+
 for i in $@
 do 
-    yum install $i -y 
+    yum list installed $i &>>$LOGFILE
+    if [ $? -ne 0 ]
+    then 
+        echo "$i is not available, pls install it"
+        yum install $i -y &>>$LOGFILE
+        VALIDATE $? "$i"
+    else
+        echo -e "$Y Package is already installed"
+    fi
 done
+
+
+
 
