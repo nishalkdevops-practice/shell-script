d65b9f7b3d904e751d794e9eff8491b3c44d4703
nishal <nishalk9399@gmail.com>
2024-08-15 12:24:43 +0530

    Conditions script

--- 09-functions.sh
+++ 09-functions.sh
@@ -6,16 +6,20 @@ DATE=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$0
 LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
 
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
 
 
 VALIDATE(){
 
 if [ $1 -ne 0 ]
 then
-    echo "$2 ERROR: Installation"
+    echo -e "$2 $R ERROR $N: Installation"
     exit1
 else
-    echo "$2 SUCCESSFUL: Installation"
+    echo -e "$2 $G SUCCESSFUL $N: Installation"
 fi
 
 }
