00fe933bee69085acc6c6fa6836e0a393381bf69
nishal <nishalk9399@gmail.com>
2024-08-15 11:28:49 +0530

    Conditions script

---
+++ 09-functions.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#this script is use to know and understand about the functions. 
+
+USERID=$(id -u)
+
+VALIDATE(){
+
+if [ $1 -ne 0 ]
+then
+    echo "$2 ERROR: Installation"
+    exit1
+else
+    echo "$2 SUCCESSFUL: Installation"
+fi
+
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR: Please run the script with the root user credentials"
+    exit1
+fi
+
+yum install mysql -y 
+
+VALIDATE $? "Installling MySQL"
+
+yum install postfix -y 
+
+VALIDATE $? "Installling Postfix"
